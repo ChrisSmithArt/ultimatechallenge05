135a3a9faf65c0b79170edb62e96c1f9899ad8f7
ChrisSmithArt <chrissmithart@proton.me>
2024-06-10 09:33:56 -0500

    changed the get route to resource method to cover all conventional routes, added a show page to display information about an individual product.

--- simple_store/app/controllers/products_controller.rb
+++ simple_store/app/controllers/products_controller.rb
@@ -2,4 +2,11 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show
+    @products = Product.find(params[:id])
+  end
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :stock_quantity)
+  end
 end
--- simple_store/config/routes.rb
+++ simple_store/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # The root of the application, this makes it so that the index page within the products view loads first.
   root "products#index"
 
-  get "/products", to: "products#index"
+  # method that maps all conventional routes for a model(collection of data/resources?)
+  resources :products
 end
